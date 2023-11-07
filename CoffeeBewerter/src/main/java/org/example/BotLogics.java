3ef1e18780656820db7221223909cf7952007aad
LevBayanov <50771555+levbayanov@users.noreply.github.com>
2023-11-07 20:41:36 +0500

    Добавил ветку

--- CoffeeBewerter/src/main/java/org/example/BotLogics.java
+++ CoffeeBewerter/src/main/java/org/example/BotLogics.java
@@ -2,20 +2,14 @@ package org.example;
 
 public class BotLogics {
 
-    public String readeMessage(String textMessage)
-    {
+    public String readeMessage(String textMessage) {
         String answer;
 
-        if ("/start".equals(textMessage))
-        {
+        if ("/start".equals(textMessage)) {
             answer = "Привет! я бот оценщик кафе";
-        }
-        else if("/grade".equals(textMessage))
-        {
+        } else if ("/grade".equals(textMessage)) {
             answer = "Напиши название кафе\n и его оценку";
-        }
-        else
-        {
+        } else {
             answer = "Я не понял";
         }
         return answer;
